ef0b48787398bc42a497e85c8250d843674808f1
Felipe Cavalcanti <felipe.cavalcanti@tfgco.com>
2016-05-26 14:51:00 -0300

    initial commit

---
+++ Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ssl_static
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libssl.a
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := crypto_static
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libcrypto.a
+include $(PREBUILT_STATIC_LIBRARY)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# openssl_prebuilt_for_android
-OpenSSL 1.0.2h prebuilt and ready for linking statically with NDK
+# Openssl 1.0.2h prebuilt for android (aermeabi, armeabi_v7)
+
+# Usage
+## TODO
